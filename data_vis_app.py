81b37c62b1701d8f33b1164f58bfd4fe7e22f87e
winfrednyoroka <winfredgatua13@gmail.com>
2024-07-16 12:37:04 +0100

    remove renaming of studies with more than 2 arms

--- data_vis_app.py
+++ data_vis_app.py
@@ -39,54 +39,7 @@ DBP['Intervention_Control'] = DBP['Intervention_Control'].str.capitalize() # Mak
 ##### Visualize baseline and post-intervention at 12 months - DBP and split some studies with more than 2 treatment groups
 df = DBP.loc[DBP['Study duration'].isin ([12,0])]
 df = df.loc[~df['pmid'].isin([34554379,194696,25919069,30471927,32805133,1234567,23706413])]# Exclude papers without 12 month measures
-# Filter Ospanov et al 
-Ospanov_LOAGB = df[(df['extracted_portion'].isin(['HDER','LOAGB-OSPAN']))]#)] and df[(df['extracted_portion'].isin(['HDER','LOAGB-OSPAN']))]# Slice the data to include just these two studies
-Ospanov_LMGB = df[(df['extracted_portion'].isin(['HDER','LMGB-OAGB']))]
-chart_LOAGB = alt.Chart(Ospanov_LOAGB).encode(alt.X('meanBMI').scale(zero=False,domain=[25, 56]),alt.Y('meanDBP').scale(zero=False,domain=[67, 96]),
-                             color= alt.Color('Intervention_Control:N').scale(domain=['Control','Intervention'],range=['blue','red']),
-                             tooltip=list(df.columns))
-LOAGB_line = chart_LOAGB.mark_line(opacity=0.9,size=3).encode()
-LOAGB_point = chart_LOAGB.mark_point(filled=True,size=200,opacity=0.9).encode(shape=alt.Shape('Study duration:N').scale(range=["circle","diamond"]))
 
-LOAGB_y =alt.Chart(Ospanov_LOAGB).mark_rule(strokeDash=[10, 10],color='green',size=3).encode(y=alt.datum(80))
-LOAGB_x =alt.Chart(Ospanov_LOAGB).mark_rule(strokeDash=[10, 10],color='gray',size=3).encode(x=alt.datum(30))
-
-chart_LMGB = alt.Chart(Ospanov_LMGB).encode(alt.X('meanBMI').scale(zero=False,domain=[25, 56]),alt.Y('meanDBP').scale(zero=False,domain=[67, 96]),
-                             color= alt.Color('Intervention_Control:N').scale(domain=['Control','Intervention'],range=['blue','red']),
-                             tooltip=list(df.columns))
-LMGB_line = chart_LMGB.mark_line(opacity=0.9,size=3).encode()
-LMGB_point = chart_LMGB.mark_point(filled=True,size=200,opacity=0.9).encode(shape=alt.Shape('Study duration:N').scale(range=["circle","diamond"]))
-
-LMGB_y =alt.Chart(Ospanov_LMGB).mark_rule(strokeDash=[10, 10],color='green',size=3).encode(y=alt.datum(80))
-LMGB_x =alt.Chart(Ospanov_LMGB).mark_rule(strokeDash=[10, 10],color='gray',size=3).encode(x=alt.datum(30))
-
-# Filter on Tur et al
-
-Tur_COT = df[(df['extracted_portion'].isin(['COT','SOG']))]# Slice the data to include just these treatment groups
-Tur_ILI = df[(df['extracted_portion'].isin(['ILI','SOG']))] # Slice to just include this treatment arms
-
-# Actual charts
-chart_COT = alt.Chart(Tur_COT).encode(alt.X('meanBMI').scale(zero=False,domain=[25, 56]),alt.Y('meanDBP').scale(zero=False,domain=[67, 96]),
-                             color= alt.Color('Intervention_Control:N').scale(domain=['Control','Intervention'],range=['blue','red']),
-                             tooltip=list(df.columns))
-COT_line = chart_COT.mark_line(opacity=0.9,size=3).encode()
-COT_point = chart_COT.mark_point(filled=True,size=200,opacity=0.9).encode(shape=alt.Shape('Study duration:N').scale(range=["circle","diamond"]))
-
-COT_y = alt.Chart(Tur_COT).mark_rule(strokeDash=[10, 10],color='green',size=3).encode(y=alt.datum(80))
-COT_x = alt.Chart(Tur_COT).mark_rule(strokeDash=[10, 10],color='gray',size=3).encode(x=alt.datum(30))
-
-chart_ILI = alt.Chart(Tur_ILI).encode(alt.X('meanBMI').scale(zero=False,domain=[25, 56]),alt.Y('meanDBP').scale(zero=False,domain=[67, 96]),
-                             color= alt.Color('Intervention_Control:N').scale(domain=['Control','Intervention'],range=['blue','red']),
-                             tooltip=list(df.columns))
-
-ILI_line = chart_ILI.mark_line(opacity=0.9,size=3).encode()
-ILI_point = chart_ILI.mark_point(filled=True,size=200,opacity=0.9).encode(shape=alt.Shape('Study duration:N').scale(range=["circle","diamond"]))
-ILI_y =alt.Chart(Tur_ILI).mark_rule(strokeDash=[10, 10],color='green',size=3).encode(y=alt.datum(80))
-ILI_x =alt.Chart(Tur_ILI).mark_rule(strokeDash=[10, 10],color='gray',size=3).encode(x=alt.datum(30))
-
-
-# Exclude Ospanov and Tur et al
-df = df.loc[~df['pmid'].isin([33963974,23163735])]# Exclude Tur and Ospanov et al from the main table
 P = alt.Chart(df).encode(alt.X('meanBMI').scale(zero=False,domain=[25, 56]),
                          alt.Y('meanDBP').scale(zero=False,domain=[67, 96]),
                          color= alt.Color('Intervention_Control:N').scale(domain=['Control','Intervention'],range=['blue','red']),
@@ -97,15 +50,10 @@ p3= alt.Chart(df).mark_rule(strokeDash=[10, 10],color='green',size=3).encode(y=a
 p4 = alt.Chart(df).mark_rule(strokeDash=[10, 10],color='gray',size=3).encode(x=alt.datum(30))
 #p4.mark_text(align='left',dx=5).encode(text='BMI')
 
-main_plot = (p1+p2+p3+p4).facet('author',columns = 2, align='all')
-
-LOAGB = (LOAGB_line + LOAGB_point + LOAGB_x  + LOAGB_y).properties(title='Ospanov_LOAGB_OSPAN vs HDER')
-LMGB = (LMGB_line + LMGB_point + LMGB_x + LMGB_y).properties(title='Ospanov_LMGB_OAGB vs HDER')
-COT =  (COT_line + COT_point + COT_x + COT_y).properties(title='Tur_COT vs SOG')
-ILI =  (ILI_line + ILI_point + ILI_x + ILI_y).properties(title='Tur_ILI vs SOG')
+main_plot = (p1+p2+p3+p4).facet('Author_pmid',columns = 2, align='all')
 
 # Combine all plots
-DBP_plot = main_plot & LMGB & LOAGB | COT & ILI # '|' combine horizontally and '&' combine vertically
+DBP_plot = main_plot
 DBP_plot.configure_axis(
     labelFontSize=20,
     titleFontSize=20
