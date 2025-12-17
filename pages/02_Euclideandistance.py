e6adc873906215502e7e19fa4718e90e5951e526
winfrednyoroka <winfredgatua13@gmail.com>
2025-12-17 14:50:20 +0000

    relative path excluding the root

--- pages/02_Euclideandistance.py
+++ pages/02_Euclideandistance.py
@@ -16,7 +16,7 @@ st.sidebar.markdown("**Euclidean distance data and visualisation**")
 
 tab1,tab2=st.tabs(['Data','Euclidean distance visualisation @baseline'])
 # Read in the data (Euclidena distance at baseline)
-result_df = pd.read_csv('/Data/DBP_baseline_normalizedeucdist.csv')
+result_df = pd.read_csv('Data/DBP_baseline_normalizedeucdist.csv')
 #result_df =pd.read_csv("https://github.com/winfrednyoroka/Data_driven_visualisation/blob/main/Data/DBP_baseline_normalizedeucdist.csv")
 # Display the dataframe
 
